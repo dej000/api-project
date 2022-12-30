c60745efc5a0e9689ea2e9c5b79f58f894a71652
dej000 <dondejavul@yahoo.com>
2022-12-30 11:59:38 +0100

    first commit

---
+++ src/components/SideNav.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar p-4 ">
+    <a class="navbar-brand mb-5" href="/"><img src="../assets/Logo (2).svg" alt=""></a>
+    <ul class="list-unstyled d-grid gap-4  " >
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Image Upload</a></li>
+        <li><a href="">Images</a></li>
+        <li><a href="">Billings</a></li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+ .sidebar{
+    color: black;
+    background: #FBFBFB;
+   float: left;
+   position: fixed;
+   z-index: 1;
+   top: 0;
+   left: 0;
+   bottom: 0;
+   display: flex;
+   flex-direction: column;
+
+ }
+
+ a{
+    text-decoration: none;
+    color: black;
+    gap: 10px;
+ }
+</style>
